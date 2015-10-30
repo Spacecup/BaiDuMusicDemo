7ccc7aecea66d5c16acbcb58cb11fa880103af26
yulili <yulili2020@qq.com>
2015-10-30 23:32:33 +0800

    搭建基本架构

--- BaiDuMusic/BaiDuMusic/Classes/Controller/MainTableViewController.h
+++ BaiDuMusic/BaiDuMusic/Classes/Controller/MainTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MainTableViewController.h
 //  BaiDuMusic
 //
 //  Created by 余丽丽 on 15/10/30.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MainTableViewController : UITableViewController
 
 @end
-
