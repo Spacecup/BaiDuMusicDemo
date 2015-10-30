54359769254c48bddd46952198f364e16d86e862
“yulili” <1248174688@qq.com>
2015-10-30 23:12:39 +0800

    Creat new project

---
+++ BaiDuMusic/BaiDuMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaiDuMusic
+//
+//  Created by 余丽丽 on 15/10/30.
+//  Copyright © 2015年 余丽丽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaiDuMusic/BaiDuMusic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaiDuMusic
+//
+//  Created by 余丽丽 on 15/10/30.
+//  Copyright © 2015年 余丽丽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
